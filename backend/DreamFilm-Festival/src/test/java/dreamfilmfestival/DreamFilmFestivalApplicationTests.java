d909a020c5625a81fccad155277be1f6cf2f82a6
Junyoung Yang <y@jun0.dev>
2025-11-25 22:50:43 +0900

    chore: 백엔드 프로젝트 초기 세팅

---
+++ backend/DreamFilm-Festival/src/test/java/dreamfilmfestival/DreamFilmFestivalApplicationTests.java
@@ -0,0 +1,13 @@
+package dreamfilmfestival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamFilmFestivalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
